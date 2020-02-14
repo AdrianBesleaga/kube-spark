19f089b5214a88217fe94277971cfe1e7d778bfc
Yifeng Jiang <uprushworld@gmail.com>
2020-02-14 14:25:58 +0800

    add endpoint to s5cmd

--- doglover/build.sh
+++ doglover/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+S3_ENDPOINT="http://192.168.170.12:80"
 JAR_DIR="s3://deephub/user/yijiang/doglover/"
 
 echo "Making jar file"
@@ -7,6 +8,6 @@ sbt package
 
 echo
 echo "Uploading to S3"
-s5cmd cp target/scala-2.12/doglover_2.12-0.1.0-SNAPSHOT.jar $JAR_DIR
+s5cmd --endpoint-url=$S3_ENDPOINT cp target/scala-2.12/doglover_2.12-0.1.0-SNAPSHOT.jar $JAR_DIR
 
 echo "Done"
