cf5c8c0526a09b571b9aeba237410eba99cfa00f
Yifeng Jiang <uprushworld@gmail.com>
2020-03-06 13:24:07 +0800

    use warehouse namespace

--- metastore/metastore-env.sh
+++ metastore/metastore-env.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Create a namespace for data warehouse
+kubectl create ns warehouse
+
 # Store S3 access key in secret
-kubectl create secret generic metastore-s3-keys \
+kubectl -n warehouse create secret generic metastore-s3-keys \
 	--from-literal=access-key='my_aws_s3_access_key' \
 	--from-literal=secret-key='my_aws_s3_secret_key'
 
 # Create configMap
-kubectl create configmap metastore-cfg \
+kubectl -n warehouse create configmap metastore-cfg \
 	--from-file=metastore-site.xml \
 	--from-file=core-site.xml
-	
